22c9be0dabcaa60a59964802f685c3d7d8056c1a
AMAN <amansrivastav1825@gmail.com>
2026-01-03 16:27:14 +0530

    Added centralised logging,metrices and tracing functionalities

--- user/src/main/java/com/ecommerce/user/controller/UserController.java
+++ user/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -3,6 +3,9 @@ package com.ecommerce.user.controller;
 import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import com.ecommerce.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +14,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
+//    private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -21,11 +26,14 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers() {
+//        logger.info("getting user");
+        log.info("getting user");
         return new ResponseEntity<>(userService.fetchAllUsers(),HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
+        log.info("user id: {} " + id);
         return userService.fetchUser(id)
                 .map(ResponseEntity :: ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
